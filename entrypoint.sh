31746a3118d324067687a4ac64b7329e35f4a276
Sergio Talens-Oliag <sto@kyso.io>
2022-08-09 08:58:58 +0200

    Tabs to spaces.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,11 @@ USERNAME="${1:-$INPUT_USERNAME}"
 TOKEN="${2:-$INPUT_TOKEN}"
 URL="${3:-$INPUT_URL}"
 if [ -z "$USERNAME" ] || [ -z "$TOKEN" ] || [ -z "$URL" ]; then
-	echo "Usage: $0 [USERNAME [TOKEN [URL]]]"
-	echo ""
-	echo "Arguments can also be passed as variables using INPUT_ as prefix."
-	exit 1
+  echo "Usage: $0 [USERNAME [TOKEN [URL]]]"
+  echo ""
+  echo "Arguments can also be passed as variables using INPUT_ as prefix."
+  exit 1
 fi
 kyso login --kysoInstallUrl "$URL" --provider "kyso" --username "$USERNAME" \
-    --token "$TOKEN"
+  --token "$TOKEN"
 kyso push
